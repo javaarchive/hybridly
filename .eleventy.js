f06c77c352756877ec31a788d81165b045e2bc06
Raymond <20248577+javaarchive@users.noreply.github.com>
2023-03-25 21:23:22 +0000

    toc test?

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,35 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
+const embedEverything = require("eleventy-plugin-embed-everything");
+
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+const markdownItEmoji = require('markdown-it-emoji');
+const pluginTOC = require('eleventy-plugin-toc');
 
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPassthroughCopy('src/img')
+  eleventyConfig.addPlugin(embedEverything);
 
+  eleventyConfig.addPassthroughCopy('src/img');
+  eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy('src/video');
+  eleventyConfig.addPassthroughCopy('src/js');
+
+  eleventyConfig.setLibrary(
+    'md',
+        markdownIt({
+            html: true,
+            linkify: true,
+            typographer: false // better not break code
+        }).use(markdownItAnchor).use(markdownItEmoji)
+  );
+
+  eleventyConfig.addPlugin(pluginTOC, {
+    tags: ['h1','h2', 'h3', 'h4', 'h5', 'h6'],
+    wrapper: 'div'
+  });
 
   const {
     DateTime
